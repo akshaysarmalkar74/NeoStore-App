7b4ad83d3aa8d5a07073556afb5d7372135e0b2f
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2022-02-02 21:20:52 -0800

    Add Custom Fonts & Colour Extension

---
+++ NeoStore Project/Utility/Extensions Group/Colors/ColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColorExtension.swift
+//  NeoStore Project
+//
+//  Created by Neosoft on 02/02/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    @nonobjc class var mainRed: UIColor {
+        return UIColor(red: 233.0/255.0, green: 26.0/255.0, blue: 28.0/255.0, alpha: 1.0)
+    }
+}
