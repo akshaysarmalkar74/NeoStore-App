d79886f5103b8e70da20c6f5c85c7e2bbb758a4c
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2022-02-08 20:44:51 -0800

    Add Reactive Listener

---
+++ NeoStore Project/Utility/ReactiveListener/ReactiveListener.swift
@@ -0,0 +1,32 @@
+//
+//  ReactiveListener.swift
+//  NeoStore Project
+//
+//  Created by Neosoft on 08/02/22.
+//
+
+import Foundation
+
+class ReactiveListener<T>{
+    typealias Listener = (T) -> ()
+    var listener: Listener?
+    
+    func bind(_ listener: Listener?){
+        self.listener = listener
+    }
+    
+    func bindAndFire(_ listener: Listener?){
+        self.listener = listener
+        listener?(value)
+    }
+    
+    var value: T{
+        didSet{
+            listener?(value)
+        }
+    }
+    
+    init(_ v: T){
+        value = v
+    }
+}
